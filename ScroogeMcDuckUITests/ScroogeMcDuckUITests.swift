7dbeb7c3c6f6ed07e9fd4e9fe18e8465284d476d
Ilya Biltuev <ilyabiltuev@gmail.com>
2022-04-04 12:38:48 +0300

    added UI test

--- ScroogeMcDuckUITests/ScroogeMcDuckUITests.swift
+++ ScroogeMcDuckUITests/ScroogeMcDuckUITests.swift
@@ -9,34 +9,40 @@ import XCTest
 
 class ScroogeMcDuckUITests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+    override func setUp() {
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    func testSalaryView() throws {
+        let grossSalaryText = "50000"
+        let netSalary1 = "30250.0"
+        let netSalary2 = "29200.0"
+        let netSalary3 = "28750.0"
+        
         let app = XCUIApplication()
         app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+        
+        let grossSalaryTextField = app.textFields["Enter salary on paper"]
+        XCTAssertTrue(grossSalaryTextField.exists)
+        grossSalaryTextField.tap()
+        grossSalaryTextField.typeText(grossSalaryText)
+        
+        let segmentedControlButton1 = app.segmentedControls.buttons["No"]
+        XCTAssertTrue(segmentedControlButton1.exists)
+        segmentedControlButton1.tap()
+        let resultLabelResult1 = app.staticTexts[netSalary1]
+        XCTAssert(resultLabelResult1.exists)
+        
+        let segmentedControlButton2 = app.segmentedControls.buttons["2.1 %"]
+        XCTAssertTrue(segmentedControlButton2.exists)
+        segmentedControlButton2.tap()
+        let resultLabelResult2 = app.staticTexts[netSalary2]
+        XCTAssert(resultLabelResult2.exists)
+        
+        let segmentedControlButton3 = app.segmentedControls.buttons["3 %"]
+        XCTAssertTrue(segmentedControlButton3.exists)
+        segmentedControlButton3.tap()
+        let resultLabelResult3 = app.staticTexts[netSalary3]
+        XCTAssert(resultLabelResult3.exists)
     }
 }
